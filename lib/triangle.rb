c0c685a298d045d666e19d8c12a594a186ae079d
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-26 22:32:33 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [@side1, @side2, @side3]
+  end
+
+  def triangle_inequality
+    if @side1 + @side2 <= @side3
+      return true
+    elsif @side2 + @side3 <= @side1
+      return true
+    elsif @side3 + @side1 <= @side2
+      return true
+    else
+      return false
+    end
+  end
+
+  def kind
+    if @sides.find {|s| s <= 0} != nil || self.triangle_inequality == true
+      raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3
+      return :equilateral
+    elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+      return :scalene
+    else
+      return :isosceles
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "THAT'S A SHITTY FUCKING TRIANGLE"
+  end
 end
